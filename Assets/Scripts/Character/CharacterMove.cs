842997d3f5ae0af7872d4f6d178ad7fb9887b6ed
4057492 <837233905@qq.com>
2020-09-08 20:50:08 +0800

    简单的移动实现

--- Assets/Scripts/Character/CharacterMove.cs
+++ Assets/Scripts/Character/CharacterMove.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
-    public float speed = 0;
-    public float acceleration = 0;
+    public float speed = 0.025f;
+    //public float acceleration = 0;
+    //public float maxSpeed = 1;
+    //private Rigidbody rigidbody;
+    int moveDir = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void Update()
+   {
+       if(moveDir != 0)
+       {
+            transform.Translate(new Vector3(moveDir*speed,0,0),Space.Self);
+       }
 
-    public void AddSpeed()
+   }
+    
+    public void Move(bool isMoveLeft)
     {
-        speed += acceleration;
-        print(speed);
+        if(isMoveLeft)
+        {
+            moveDir = -1;
+        }
+        else
+        {
+            moveDir = 1;
+        }
+    }
+    public void StopMove()
+    {
+        moveDir = 0;
     }
 }
