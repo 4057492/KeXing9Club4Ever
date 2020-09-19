f2c06dd68e94bb5a35a616e905b097fbdd0e2d84
4057492 <837233905@qq.com>
2020-09-20 01:22:50 +0800

    demo框架完成
    因为懒得搞程序集引用 干脆把dialogue editor代码放到项目代码文件夹里

---
+++ Assets/Scripts/UI/OnConversationUIChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class OnConversationUIChange : MonoBehaviour
+{
+    public SetMultiGameobjecsActive MainHUBControl;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        ConversationManager.OnConversationStarted += OnConversationStart;
+        ConversationManager.OnConversationEnded += OnConversationEnd;
+    }
+    void OnDisable()
+    {
+        ConversationManager.OnConversationStarted -= OnConversationStart;
+        ConversationManager.OnConversationEnded -= OnConversationEnd;
+    }
+
+    void OnConversationStart()
+    {
+        if(MainHUBControl)
+        {
+            MainHUBControl.SetTargetsActive(false);
+        }
+    }
+
+    void OnConversationEnd()
+    {
+        if (MainHUBControl)
+        {
+            MainHUBControl.SetTargetsActive(true);
+        }
+    }
+
+}
