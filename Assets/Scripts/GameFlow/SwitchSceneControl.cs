93d2a2e795d0260ebc339501942dcc62ee85ee24
zintrulcre <zintrulcre@gmail.com>
2020-09-28 21:37:20 +0800

    feat: switch scene control

---
+++ Assets/Scripts/GameFlow/SwitchSceneControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchSceneControl : MonoBehaviour {
+    public string roomName;
+
+    private void OnTriggerEnter(Collider roomEdge)
+    {
+        if (roomEdge.CompareTag("Player"))
+            SceneManager.LoadScene(roomName);
+    }
+}
