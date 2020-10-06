5539b991fd74d359ec88de87af0210fa1fd8309c
4057492 <837233905@qq.com>
2020-10-07 00:51:04 +0800

    角色传送以及场景转换的修改 UI相关修改

---
+++ Assets/Scripts/Character/CharacterTele.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTele : MonoBehaviour
+{
+    public void Tele(Transform target)
+    {
+        if(target)
+        {
+            Vector3 temp = target.position;
+            temp.z = transform.position.z;
+            transform.position = temp;
+        }
+    }
+}
