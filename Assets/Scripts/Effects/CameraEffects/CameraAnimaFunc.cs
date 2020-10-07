c77eda3c4a19586bc8e682f5612f4af2a1ef3c6d
4057492 <837233905@qq.com>
2020-10-07 13:51:36 +0800

    镜头效果以及条件触发器

---
+++ Assets/Scripts/Effects/CameraEffects/CameraAnimaFunc.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimaFunc : MonoBehaviour
+{
+    Animator animator;
+    float effectTime = 2.0f;
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    public void StartEffect()
+    {
+        animator.SetBool("Start",true);
+        animator.SetBool("Over",false);
+    }
+    public void StopEffect()
+    {
+        animator.SetBool("Start",false);
+        animator.SetBool("Over",true);
+    }
+    public void ResetEffect()
+    {
+        animator.SetBool("Start",false);
+        animator.SetBool("Over",false);
+    }
+
+    public void Effect()
+    {
+        EffectWithTime(2.0f);
+    }
+
+    public void EffectWithTime(float time)
+    {
+        effectTime = time;
+        StopAllCoroutines();
+        StartCoroutine(CameraEffectCoroutine());
+    }
+
+    IEnumerator CameraEffectCoroutine()
+    {
+        StartEffect();
+        yield return new WaitForSecondsRealtime (effectTime);
+        StopEffect();
+    }
+}
---
+++ Assets/Scripts/GameFlow/ConditionTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using UnityEngine.Serialization;
+
+public class ConditionTrigger : MonoBehaviour
+{
+    public bool[] conditions;
+
+    [Serializable]
+    public class ConditionEvent : UnityEvent {}
+
+    
+    [FormerlySerializedAs("onCondition")]
+    [SerializeField]
+    private ConditionEvent m_OnCondition = new ConditionEvent();
+
+    public ConditionEvent onEnter
+    {
+        get { return m_OnCondition; }
+        set { m_OnCondition = value; }
+    }   
+
+    public void SetCondition(int index)
+    {
+        if(index >= 0 && index < conditions.Length)
+        {
+            conditions[index] = true;
+        }
+        CheckIfShouldTrigger();
+    }
+
+    void CheckIfShouldTrigger()
+    {
+        bool result = true;
+        foreach(bool condition in conditions)
+        {
+            result = result & condition;
+        }
+        if(result)
+        {
+            m_OnCondition.Invoke();
+        }
+    }
+
+
+}
